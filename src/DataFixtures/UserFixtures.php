fe2727384980e2a0d176c085a4c6fd380d3c78d2
etienne_lejariel <etienne.lejariel@wonderbox.fr>
2019-10-26 09:49:27 +0200

    First commit

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+
+
+        $user = new User();
+        $user->setEmail('pitlejariel@hotmail.com');
+        $user->setPassword($this->passwordEncoder->encodePassword($user,'password'));
+
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
